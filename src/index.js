72d8a1a9dd30374d99296e3625e8a03f513adb2d
Murithi <murithier.2006@gmail.com>
2017-11-14 15:32:58 +0300

    user authentication

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import path from 'path';
-const app = express();
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import Promise from 'bluebird';
 
-app.post('/api/auth', (req, res) => {
-  res.status(400).json({ errors: { global: 'Invalid credentials' } });
-});
+import auth from './routes/auth';
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.json());
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
+
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(8080, () => console.log('App Listening on localhost://8080'));
